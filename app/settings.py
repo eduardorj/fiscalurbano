af15ce763614b10c720abde2e09bb7dd8060cc22
Eduardo Francellino <edfrancell@gmail.com>
2014-06-01 02:37:54 -0300

    fixdb

--- app/settings.py
+++ app/settings.py
@@ -69,17 +69,15 @@ ROOT_URLCONF = 'app.urls'
 WSGI_APPLICATION = 'app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+#DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 
 # Internationalization
