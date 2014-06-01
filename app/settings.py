c9d501e092e3b02ecb003310e67bd77bcd97a1a0
Eduardo Francellino <edfrancell@gmail.com>
2014-06-01 02:11:00 -0300

    fix settings

--- app/settings.py
+++ app/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
