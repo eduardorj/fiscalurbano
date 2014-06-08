d7fdb7badd66538218fc790bc37801a6d3d56e47
Eduardo Francellino <edfrancell@gmail.com>
2014-06-08 14:46:01 -0300

    fix pagination

--- app/fiscalizacao/views.py
+++ app/fiscalizacao/views.py
@@ -8,12 +8,12 @@ from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 
 
-class RelatoViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Relato.objects.all()
-    serializer_class = RelatoSerializer
+#class RelatoViewSet(viewsets.ModelViewSet):
+#    """
+#    API endpoint that allows users to be viewed or edited.
+#    """
+#    queryset = Relato.objects.all()
+#    serializer_class = RelatoSerializer
 
 class RelatoList(generics.ListCreateAPIView):
     queryset = Relato.objects.all()
--- app/urls.py
+++ app/urls.py
@@ -9,14 +9,17 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-router = routers.DefaultRouter()
+#router = routers.DefaultRouter()
 #router.register(r'relatos', views.RelatoViewSet)
 
 urlpatterns = patterns('',
     (r'^$', homepage),
-    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^relatos/', generics.ListCreateAPIView.as_view(model=Relato)),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
+
+
+#url(r'^', include(router.urls)),
+    
