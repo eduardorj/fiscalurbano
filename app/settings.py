20727bc40057f44d9343ecc1f49d9937cc4a602e
Eduardo Francellino <edfrancell@gmail.com>
2014-06-13 23:52:37 -0300

    add realy without auth

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     }
 }
 
-#DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
+DATABASES = { 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) }
 
 
 # Internationalization
