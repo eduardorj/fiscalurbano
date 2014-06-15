b64f59a96f613e749ad28fd07c7023cb104b37c3
Eduardo Francellino <edfrancell@gmail.com>
2014-06-15 01:08:38 -0300

    fix encoding

--- app/settings.py
+++ app/settings.py
@@ -53,8 +53,13 @@ INSTALLED_APPS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.UnicodeJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+
 }
-#    'PAGINATE_BY': 100
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
